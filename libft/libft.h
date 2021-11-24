256157affb761ea3537f632b95ea18d43c4c4add
tnaceur <tnaceur@student.1337.ma>
2021-11-24 18:26:58 +0100

    42

--- libft/libft.h
+++ libft/libft.h
@@ -50,6 +50,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_lstadd_front(t_list **alst, t_list *new);
+void	ft_lstadd_back(t_list **alst, t_list *new);
 
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
